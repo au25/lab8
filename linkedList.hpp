7a778284ffd754e7f6fbda74537a6eeef200fb8c
Oscar <au.oscar@hotmail.com>
2019-11-22 22:28:51 -0800

    finished - with bugs

--- linkedList.hpp
+++ linkedList.hpp
@@ -2,6 +2,97 @@
 // Created by Oscar Au on 2019-11-21.
 //
 
+#include <iostream>
+
 #pragma once
 
+template<typename T>
+class Node {
+private:
+    T data;
+    Node* nextNode;
+    Node* prevNode;
+public:
+    Node(){
+        data = NULL;
+        nextNode = NULL;
+        prevNode = NULL;
+    }
+    explicit Node(T data) : data{data}{
+        nextNode = NULL;
+        prevNode = NULL;
+    };
+    ~Node() = default;
+    T getData(){return data;}
+    void setData(T newValue){this->data = newValue;}
+    Node* getNext(){return nextNode;};
+    void setNext(Node* nextNode){this->nextNode = nextNode;}
+    Node* getPrev(){ return prevNode;};
+};
 
+template<typename T>
+class MyLinkedList {
+private:
+    Node<T>* head;
+public:
+    MyLinkedList(){head->setNext(NULL);}
+    ~MyLinkedList(){};
+
+    Node<T>* getHead(){return head;}
+
+    void pushFront(T data){
+        auto *newNode = new Node<T>(data);
+        newNode->setNext(head);
+        head = newNode;
+    }
+
+    void pushBack(T data){
+        auto *newNode = new Node<T>(data);
+
+        //set pointer to head
+        Node<T>* last;
+
+        if(head->getNext() == NULL){
+            last = head;
+            head->setData(data);
+        }
+
+        while(last->getNext() != NULL){
+            last = last->getNext();
+        }
+        last->setData(data);
+        last->setNext(newNode);
+        newNode->setData(NULL);
+    }
+
+    void insert(Node<T>* const& prevNode, T data){
+        auto *newNode = new Node<T>(data);
+        newNode->setNext(prevNode->getNext());
+        prevNode->setNext(newNode);
+    }
+
+    Node<T>* getPrevNode(T data){
+        Node<T>* prevNode = head;
+
+        while(prevNode->getNext() != NULL){
+            if (prevNode->getData() == data){
+                return prevNode;
+            }
+            prevNode = prevNode->getNext();
+        }
+    }
+};
+
+template<typename DLL, typename T>
+void print(MyLinkedList<DLL> dll){
+    Node<T> *node;
+    node = dll.getHead();
+    while (node->getData() != NULL){
+        if (node->getData() != NULL){
+            std::cout << node->getData() << " " << std::endl;
+            std::cout.flush();
+            std::cout.clear();
+        }
+        node = node->getNext();
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,102 +1,10 @@
-#include <iostream>
-#include <string>
+#include "linkedList.hpp"
 
 using namespace std;
 
-template<typename T>
-class Node {
-private:
-    T data;
-    Node* nextNode;
-    Node* prevNode;
-public:
-    Node(){
-        data = NULL;
-        nextNode = NULL;
-        prevNode = NULL;
-    }
-    explicit Node(T data) : data{data}{
-        nextNode = NULL;
-        prevNode = NULL;
-    };
-    ~Node() = default;
-    T getData(){return data;}
-    void setData(T newValue){this->data = newValue;}
-    Node* getNext(){return nextNode;};
-    void setNext(Node* nextNode){this->nextNode = nextNode;}
-    Node* getPrev(){ return prevNode;};
-    T getValue(){return data;}
-};
-
-template<typename T>
-class MyLinkedList {
-private:
-    Node<T>* head;
-public:
-    MyLinkedList(){head->setNext(NULL);}
-    MyLinkedList(Node<T>* node) : head{node}{};
-    ~MyLinkedList(){};
-
-    Node<T>* getHead(){return head;}
-
-    void pushFront(T data){
-        auto *newNode = new Node<T>(data);
-        newNode->setNext(head);
-        head = newNode;
-    }
-
-    void pushBack(T data){
-        auto *newNode = new Node<T>(data);
-
-        //set pointer to head
-        Node<T>* last;
-
-        if(head->getNext() == NULL){
-            last = head;
-            head->setData(data);  // head data = 3
-        }
-
-        //get last to point to last node
-        while(last->getNext() != NULL){
-            last = last->getNext();  // last points to second empty node
-        }
-        last->setNext(newNode);
-    }
-
-    void insert(Node<T>* const& prevNode, T data){
-        auto *newNode = new Node<T>(data);
-        newNode->setNext(prevNode->getNext());
-        prevNode->setNext(newNode);
-
-    }
-
-    Node<T>* getPrevNode(T data){
-        Node<T>* prevNode = head;
-
-        while(prevNode->getNext() != NULL){
-            if (prevNode->getData() == data){
-                return prevNode;
-            }
-            prevNode = prevNode->getNext();
-        }
-    }
-};
-
-template<typename DLL, typename T>
-void print(MyLinkedList<DLL> dll){
-    Node<T> *node;
-    node = dll.getHead();
-    while (node->getData() != NULL){
-        cout << node->getData() << endl;
-        node = node->getNext();
-    }
-}
 
 
 int main() {
-//    Node<int> head(10);
-//    Node<int> middle(20);
-//    Node<int> tail(30);
 
     MyLinkedList<int> intList;
     intList.pushBack(3);
@@ -104,9 +12,11 @@ int main() {
     intList.insert(intList.getPrevNode(1), 2);
     print<int, int>(intList);
 
-    MyLinkedList<string> stringList;
-    stringList.pushBack(reinterpret_cast<const char *>('c'));
-    print<string, string>(stringList);
+    MyLinkedList<char> charList;
+    charList.pushBack('c');
+    charList.pushFront('a');
+    charList.insert(charList.getPrevNode('a'), 'b');
+//    print<char, char>(charList);
     return 0;
 }
 
